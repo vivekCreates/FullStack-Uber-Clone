38cd553300e2527ea0514dc5fa84cbe10a448489
vivek <vivek@gmail.com>
2025-01-23 21:09:52 +0530

    add refreshAccessToken endpoint to handle token refresh logic

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -4,6 +4,8 @@ import { ApiError } from '../utils/ApiError';
 import { User, UserShape } from '../models/user.model';
 import { ApiResponse } from '../utils/ApiResponse';
 import { generateAccessAndRefreshTokens } from '../utils/generateTokens';
+import jwt from 'jsonwebtoken';
+
 
 interface Options {
     httpOnly: boolean;
@@ -11,7 +13,7 @@ interface Options {
     sameSite: "strict" | "none" | "lax";
 }
 
-const options:Options = {
+const options: Options = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "strict",
@@ -80,7 +82,7 @@ const loginUser = asyncHandler(async (req: Request, res: Response) => {
     }
 
     const isPasswordValid = await user.isPasswordCorrect(password)
-    console.log("isPasswordValid",isPasswordValid)
+    console.log("isPasswordValid", isPasswordValid)
     if (!isPasswordValid) {
         throw new ApiError(
             400,
@@ -89,9 +91,9 @@ const loginUser = asyncHandler(async (req: Request, res: Response) => {
 
     }
 
-    const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user?._id)
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user?._id)
 
-    if (!(accessToken|| refreshToken)) {
+    if (!(accessToken || refreshToken)) {
         throw new ApiError(400, "token not generated")
     }
 
@@ -114,32 +116,32 @@ const logoutUser = asyncHandler(async (req: Request, res: Response) => {
         req.user?._id,
         {
             $unset: {
-                refreshToken:1
+                refreshToken: 1
             }
         },
-        {new:true}
-     )
-    res
-    .clearCookie("accessToken",options)
-    .clearCookie("refreshToken",options)
-    .json(
-        new ApiResponse(
-            200,
-            user,
-            "user logout successfully"
-        )
+        { new: true }
     )
+    res
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(
+            new ApiResponse(
+                200,
+                user,
+                "user logout successfully"
+            )
+        )
 })
 
 
-const getUserProfile = asyncHandler(async(req:Request,res:Response)=>{
+const getUserProfile = asyncHandler(async (req: Request, res: Response) => {
     if (!req.user) {
-        throw new ApiError(401,"No authorized User")
+        throw new ApiError(401, "No authorized User")
     }
 
     const user = await User.findById(req.user?.id)
     if (!user) {
-        throw new ApiError(401,"user does not exist")
+        throw new ApiError(401, "user does not exist")
     }
 
     return res.json(
@@ -151,9 +153,55 @@ const getUserProfile = asyncHandler(async(req:Request,res:Response)=>{
     )
 })
 
+
+const refreshAccessToken = asyncHandler(async (req: Request, res: Response) => {
+
+    const incomingRefreshToken = req.cookies?.refreshToken || req.body?.refreshToken
+    if (!incomingRefreshToken) {
+        throw new ApiError(400, "No token found")
+    }
+
+    const decoded = jwt.verify(incomingRefreshToken, process.env.JWT_REFRESH_TOKEN_SECRET!) as { _id: string }
+    if (!decoded) {
+        throw new ApiError(400, "No User found while decoding through jsonwebtoken")
+    }
+
+    const user = await User.findById(decoded?._id)
+
+    if (!user) {
+        throw new ApiError(400, "No User found from decoded user id")
+    }
+
+    const { accessToken, refreshToken: newRefreshToken } = await generateAccessAndRefreshTokens(user._id)
+
+    if (!(accessToken || newRefreshToken)) {
+        throw new ApiError(400, "Tokens not generated")
+    }
+
+    if (newRefreshToken) {
+        user.refreshToken = newRefreshToken
+    }
+
+    user.save({ validateBeforeSave: false })
+
+    return res.json(
+        new ApiResponse(
+            200,
+            {
+                accessToken,
+                refreshToken: newRefreshToken
+            },
+            "Access and Refresh token successfully"
+        )
+    )
+
+})
+
+
 export {
     registerUser,
     loginUser,
     logoutUser,
-    getUserProfile
+    getUserProfile,
+    refreshAccessToken
 }
